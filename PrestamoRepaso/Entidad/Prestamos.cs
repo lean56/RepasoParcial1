b8f81eb2a23b72d4804e64780bc3f0c971f68f5e
leonardo emil <theflaco56@hotmail.com>
2020-02-05 10:14:18 -0400

    Entidad prestamos

--- PrestamoRepaso/Entidad/Prestamos.cs
+++ PrestamoRepaso/Entidad/Prestamos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,21 @@ namespace PrestamoRepaso.Entidad
 {
     public class Prestamos
     {
+        [Key]
+        public int PrestamosId { get; set; }
+        [Range(minimum:100,maximum:50000)]
+        public int Monto { get; set; }
+        [Range(minimum: 1, maximum: 52)]
+
+        public int Semanas { get; set; }
+        public int Cuota { get; set; }
+
+        public Prestamos()
+        {
+            PrestamosId = 0;
+            Monto = 0;
+            Semanas = 0;
+            Cuota = 0;
+        }
     }
 }
